282940a4c489e6abd82c6d97dbbf201417893070
BahngJH <wpxm2003@naver.com>
2019-11-16 18:08:19 +0900

    react init setting and material 하기전

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
+
+const customers = [{
+  'id' : 1,
+  'image' : 'https://placeimg.com/64/64/1',
+  'name' : '성순',
+  'birthday' : '특징:븅신',
+  'gender' : '남자',
+  'job' : '프로그래머',
+},
+{
+  'id' : 2,
+  'image' : 'https://placeimg.com/64/64/2',
+  'name' : '이순신',
+  'birthday' : '880322',
+  'gender' : '남자',
+  'job' : '명장',
+},{
+  'id' : 3,
+  'image' : 'https://placeimg.com/64/64/3',
+  'name' : '아이린',
+  'birthday' : '940712',
+  'gender' : '여자',
+  'job' : '아이돌',
+}]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    //<Customer id={customes.id} image={customes.image} name={customes.name} birthday={customes.birthday} gender={customes.gender} job={customes.job}/>
+    <div>
+      { customers.map(c => { return ( <Customer key={c.id} id={c.id} name={c.name} image={c.image} job={c.job} gender ={c.gender} birthday={c.birthday} />)})}
     </div>
   );
 }
